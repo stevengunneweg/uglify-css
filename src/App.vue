bb10a9036dd223145f86f59932b8efa121cf7e09
Steven <steven.gunneweg@newstory.nl>
2025-05-16 14:19:59 +0200

    feat(adding): Update app template

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,12 @@ import ButtonComponent from './components/ButtonComponent.vue';
 
 <template>
 	<HeaderComponent title="Uglify Tailwind CSS" />
-
-	<div class="flex justify-center gap-2">
-		<a href="https://vite.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
-	</div>
 	<Filler />
 	<div class="flex flex-wrap justify-center gap-2">
 		<CardComponent cardTitle="Card Title" />
 		<CardComponent cardTitle="Card Title 2" />
 		<CardComponent cardTitle="Card Title 3" />
 	</div>
-
 	<ButtonComponent buttonText="Button" />
 	<FooterComponent msg="Footer" />
 </template>
-
-<style scoped>
-</style>
