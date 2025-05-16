9b189cfd352aa418eb134b10d17fb3f3c300e3f8
Steven <steven.gunneweg@newstory.nl>
2025-05-16 09:49:00 +0200

    feat(uglify): Add method to uglify value

--- tools/scripts/uglify-css.js
+++ tools/scripts/uglify-css.js
@@ -8,3 +8,24 @@ files.forEach((file) => {
 	const fileContents = readFileSync(file, 'utf-8');
 	console.log(fileContents);
 });
+
+const mapping = {};
+const uglies = [];
+function uglifyValue(value) {
+	if (mapping[value]) {
+		return mapping[value];
+	}
+
+	const uglyValue = value.replace(/[-+]/g, '_');
+	if (uglies.includes(uglyValue)) {
+		return uglyValue + Math.floor(Math.random() * 1000);
+	}
+	uglies.push(uglyValue);
+	mapping[value] = uglyValue;
+	return uglyValue;
+}
+
+console.log('uglify-css', uglifyValue('bg-red-100'));
+console.log('uglify-css', uglifyValue('bg-red-500'));
+console.log('uglify-css', uglifyValue('bg-red-500'));
+console.log('uglify-css', uglifyValue('bg+red+500'));
