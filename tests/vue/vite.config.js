fc8ba1fd88ed83a694120ddbb3b1277af5b32925
Steven <steven.gunneweg@newstory.nl>
2025-05-20 14:18:20 +0200

    ci(cli): Restore Vue cache-busting

--- tests/vue/vite.config.js
+++ tests/vue/vite.config.js
@@ -4,14 +4,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [vue(), tailwindcss()],
-	// @TODO: Remove as this breaks cache busting but is used in meantime to be able to see a diff before/after the uglify process
-	build: {
-		rollupOptions: {
-			output: {
-				entryFileNames: 'assets/[name].js',
-				chunkFileNames: 'assets/[name].js',
-				assetFileNames: 'assets/[name].[ext]',
-			},
-		},
-	},
 });
